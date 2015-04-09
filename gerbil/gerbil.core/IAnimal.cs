beb34683f8e91a6fe2cc4c5cb68a03781518fc43
Marvin Varela <marvin.varela@outlook.com>
2015-04-09 14:39:57 -0600

    * Adding plug-in interface * Adding animal interface

---
+++ gerbil/gerbil.core/IAnimal.cs
@@ -0,0 +1,15 @@
+﻿namespace Gerbil
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IAnimal
+    {
+        Guid Id { get; set; }
+        string SpeciesName { get; set; }
+        string CommonName { get; set; }
+    }
+}
---
+++ gerbil/gerbil.core/IGerbilPlugin.cs
@@ -0,0 +1,13 @@
+﻿namespace Gerbil
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IGerbilPlugin
+    {
+        IEnumerable<IAnimal> GetAnimals();
+    }
+}
