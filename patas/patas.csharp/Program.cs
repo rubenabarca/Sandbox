d797e9b2a62e45924f3348ea04516b5fafa4d0b1
Marvin Varela <marvin.varela@outlook.com>
2015-05-10 08:52:56 -0600

    * [patas] adding C# implementation for reference

---
+++ patas/patas.csharp/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace patas.csharp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = (double)(1 + 1 / Math.Sqrt(2)) / 2;
+            var c = (double)(1 / (8 * a));
+            var s = (double)Math.Pow(a, 2);
+            var f = (double)1.0;
+            var precision = 1e-99999;
+            while (Math.Abs(c) > precision)
+            {
+                a = (a + Math.Sqrt((a - c) * (a + c))) / 2.0;
+                c = (Math.Pow(c, 2)) / (4.0 * a);
+                f = 2.0 * f;
+                s = s - (f * Math.Pow(c, 2.0));
+            }
+            Console.WriteLine((2.0 * Math.Pow(a, 2.0)) / ((2.0 * s) - 1.0));
+            Console.ReadLine();
+        }
+    }
+}
--- patas/patas/Program.fs
+++ patas/patas/Program.fs
@@ -1,33 +1,19 @@
-﻿let nthroot n A =
-    let rec f x =
-        let m = n - 1.
-        let x' = (m * x + A/x**m) / n
-        match abs(x' - x) with
-        | t when t < abs(x * 1e-9) -> x'
-        | _ -> f x'
-    f (A / double n)
-
-let geometricMean n = 
-    let seriesProduct = List.fold (*) 1.0 [1.0..n]
-    let seriesCount = float(Seq.length [1.0..n])
-    nthroot seriesCount seriesProduct
-
-let rec agm a g precision  =
-    if precision > abs(a - g) then a else
-    agm (0.5 * (a + g)) (sqrt (a * g)) precision
-
-open System
+﻿open System
 
 [<EntryPoint>]
 let main args =
-    if args.Length <> 2 then
-        eprintfn "usage: nthroot n A"
-        exit 1
-    let (b, n) = System.Double.TryParse(args.[0])
-    let (b', A) = System.Double.TryParse(args.[1])
-    if (not b) || (not b') then
-        eprintfn "error: parameter must be a number"
-        exit 1
-    printf "%A" (nthroot n A)
+    let mutable a = (1.+1./sqrt(2.))/2.
+    let mutable c = 1./(8.*a)
+    let mutable s = a**2.
+    let mutable  f = 1.
+    let precision = 0.00000000001
+    let mutable absC = abs(c)
+    while absC < precision do
+        a <- (a+sqrt((a-c)*(a+c)))/2.
+        c <- (c**2.) / (4.*a)
+        f <- 2.*f
+        s <- s-f*c**2.
+        absC <- abs(c)
+        
     Console.ReadLine() |> ignore
     0
