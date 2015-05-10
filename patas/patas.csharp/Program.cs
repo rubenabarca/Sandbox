831bf69644c4bf3cdedc66883c707a2b56ad525f
Marvin Varela <marvin.varela@outlook.com>
2015-05-10 16:10:21 -0600

    * [patas] more memory efficient implementation of modular expoentiation in C#

--- patas/patas.csharp/Program.cs
+++ patas/patas.csharp/Program.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
@@ -9,14 +10,34 @@
     class Program
     {
 
-        static double ModularExponentiation(double b, double e, double m)
+        static double ModularExponentiationDirect(double b, double e, double m)
         {
             return Math.Pow(b, e) % m;
         }
 
+        static double ModularExponentiation(double b, double e, double m)
+        {
+            var c = 1.0;
+            for (int k = 0; k < e; k++)
+            {
+                c = (b * c) % m;
+            }
+            return c;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine(ModularExponentiation(5,3,13));
+            var stopwatch = new Stopwatch();
+            double b = 18.0, e = 36.0, m = 497.0;
+            stopwatch.Start();
+            var c = ModularExponentiationDirect(b, e, m);
+            stopwatch.Stop();
+            Console.WriteLine("calculated modular exponentiation directly in {0}: {1}", stopwatch.Elapsed, c);
+            stopwatch.Reset();
+            stopwatch.Start();
+            c = ModularExponentiation(b, e, m);
+            stopwatch.Stop();
+            Console.WriteLine("calculated modular exponentiation alternate method in {0}: {1}", stopwatch.Elapsed, c);
             Console.ReadLine();
         }
     }
--- patas/patas/Program.fs
+++ patas/patas/Program.fs
@@ -2,7 +2,9 @@
 
 
 let nthPiDigit k = 
-    (1./(16.**k)) * (4./(8.*k+1.) - 2./(8.*k+4.) - 1./(8.*k+5.) - 1./(8.*k+6.))
+    (1./(16.**k)) * (4./(8.*k+1.) - 2./(8.*k+4.) - 1./(8.*k+5.) - 1./(8.*k+6.));;
+
+let modularExponentiation b e m = (b**e)%m
 
 [<EntryPoint>]
 let main args =
