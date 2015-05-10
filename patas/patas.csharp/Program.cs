19641270e1c9b1315d6304775995163086d8f631
Marvin Varela <marvin.varela@outlook.com>
2015-05-10 16:42:35 -0600

    * [patas] adding more steps

--- patas/patas.csharp/Program.cs
+++ patas/patas.csharp/Program.cs
@@ -25,22 +25,25 @@
             return c;
         }
 
-        static double GetPiCalc(double k, double n)
+        static double GetPiCalc(double k, double n, double l)
         {
-            return ModularExponentiation(16.0, n - k, 8 * k + 1) / (8 * k + 1);
+            return ModularExponentiation(16.0, n - k, 8 * k + l) / (8 * k + l);
         }
 
         static void Main(string[] args)
         {
             var stopwatch = new Stopwatch();
-            double n = 1000000.0;
+            double n = 0.0;
             stopwatch.Start();
 
             double k = 0.0;
             for (int i = 0; i <= n; i++)
             {
-                k += GetPiCalc(i, n) % 1.0;
+                k += 4 * GetPiCalc(i, n, 1) - 2 * GetPiCalc(i, n, 4) - GetPiCalc(i, n, 5) - GetPiCalc(i, n, 6);
             }
+
+            var integerPart = (int)k;
+            k = 16 * (k % integerPart);
 
             stopwatch.Stop();
             Console.WriteLine("calculated Nth digit of P in {0}: {1}", stopwatch.Elapsed, k);
