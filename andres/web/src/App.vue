cd309f9823399918393ced52f451357c897134c0
romero1504 <47726863+romero1504@users.noreply.github.com>
2020-09-23 09:20:46 -0600

    Andres (#2)
    * Evaluation/Andres
    
    * Correccion Web Vue.js
    
    * Modificacion con la coneccion al host API
    
    * Cambios en API Routing
    
    * nombre carpetas
    
    * API&Web connecting Enable CORS
    
    * modificaciones carpeta web y api
    
    * API Conectado con Web Folder
    
    * modificacion main.ts

---
+++ andres/web/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+   <holamundo/>
+  </div>
+</template>
+
+<script>
+import holamundo from './components/holamundo'
+export default {
+  name: 'App',
+  components: {
+    holamundo
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+}
+</style>
