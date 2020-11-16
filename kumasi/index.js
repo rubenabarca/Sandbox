7ecee446d424913d73a0e7e3356eb2a8118ee7f9
Rubén Abarca <rudavid0400@gmail.com>
2020-11-16 17:54:33 -0600

    Last Changes

--- kumasi/index.js
+++ kumasi/index.js
@@ -8,29 +8,7 @@ function setupProcessing(){
 
 function drawProcessing(){
     background(64);
-    
-    setInterval(function(){ 
-        const shapeindex=(int)(Math.random()*3);
-        const ramdomr=(int)(Math.random()*255);
-        const ramdomg=(int)(Math.random()*255);
-        const ramdomb=(int)(Math.random()*255);
-        fill(ramdomr,ramdomg,ramdomb);
-        switch(shapeindex){
-    
-            case 0:
-            ellipse(mouseX, mouseY, 20, 20);
-            break;
-            case 1:
-            triangle(mouseX,mouseY,mouseX+20,mouseY-20,mouseX+20,mouseY+20);
-            break;
-            case 2:
-            rect(mouseX,mouseY,20,20);
-            break;
-        }
-        
-    }, 16);
-    
-
+    ellipse (45,76,45,76);
     
     document.getElementById("label").innerHTML = "Mouse coordinates: " + mouseX + ", " + mouseY;
 }
