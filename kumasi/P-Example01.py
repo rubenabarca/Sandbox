54eb7ec47fc9f72ace61f0e6e2c4f938cf9a2675
Rubén Abarca <rudavid0400@gmail.com>
2021-02-01 20:13:28 -0600

    Last Changes

---
+++ kumasi/P-Example01.py
@@ -0,0 +1,13 @@
+x=5
+x/=5
+y=6
+if x>6:
+  print("equis es mayor a 6")
+  print(x)
+else:
+    print(x)
+first_value=25
+second_value=2
+third_value=pow(first_value,second_value)
+print(third_value)
+
